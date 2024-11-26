0bcfed8929cd938c664874ab756c4834a3faafba
Leslie <Kooliesk@gmail.com>
2024-11-26 18:42:38 +0200

    configure login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,30 +1,5 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-  # respond_to :json
+  respond_to :json
 
   private
 
@@ -34,5 +9,9 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_to_on_destroy
     head :no_content
+  end
+
+  def respond_to_on_failure
+    render json: { error: "Invalid email or password" }, status: :unauthorized
   end
 end
