d32cbc6baa4fb5d730b8947e7f1f19695ed9fbdc
Leslie <Kooliesk@gmail>
2025-08-18 22:33:53 +0200

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000" # Change this to the domain of your frontend app in production
+    origins "https://lawyer-connect.netlify.app/" # Change this to the domain of your frontend app in production
 
     resource "*",
       headers: :any,
